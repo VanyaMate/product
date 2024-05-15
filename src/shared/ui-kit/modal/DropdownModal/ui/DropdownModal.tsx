f132e76a082b382ed2b022ffebf4300087a56b4b
VanyaMate <buhu653@mail.ru>
2024-05-16 00:51:31 +0300

    start to creating dropdownmodal

---
+++ src/shared/ui-kit/modal/DropdownModal/ui/DropdownModal.tsx
@@ -0,0 +1,60 @@
+import {
+    ComponentPropsWithoutRef,
+    FC,
+    memo,
+    ReactNode,
+    useEffect,
+    useRef,
+} from 'react';
+import classNames from 'classnames';
+import css from './DropdownModal.module.scss';
+import {
+    IModalController,
+} from '@/shared/ui-kit/modal/Modal/hooks/useModalController.ts';
+import { createPortal } from 'react-dom';
+
+
+export type DropdownModalProps =
+    {
+        dropdown: ReactNode;
+        controller: IModalController;
+    }
+    & ComponentPropsWithoutRef<'div'>;
+
+export const DropdownModal: FC<DropdownModalProps> = memo(function DropdownModal (props) {
+    const { className, children, dropdown, controller, ...other } = props;
+
+    const placeRef    = useRef<HTMLDivElement>(null);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (controller.opened) {
+            dropdownRef.current.style.height = dropdownRef.current.scrollHeight + 'px';
+            const placeClientRect            = placeRef.current.getBoundingClientRect();
+            dropdownRef.current.style.top    = (placeClientRect.bottom + 15) + 'px';
+            dropdownRef.current.style.left   = (placeClientRect.right - dropdownRef.current.scrollWidth) + 'px';
+        } else {
+            dropdownRef.current.style.height = '0px';
+        }
+    }, [ controller.opened ]);
+
+    return (
+        <div
+            { ...other }
+            className={ classNames(css.container, {}, [ className ]) }
+        >
+            <div ref={ placeRef }>
+                { children }
+            </div>
+            {
+                createPortal(
+                    <div className={ classNames(css.dropdown, {}, []) }
+                         ref={ dropdownRef }>
+                        { dropdown }
+                    </div>,
+                    document.body,
+                )
+            }
+        </div>
+    );
+});
--- src/widgets/site/navigation/HeaderNavBar/ui/HeaderNavBar.tsx
+++ src/widgets/site/navigation/HeaderNavBar/ui/HeaderNavBar.tsx
@@ -11,12 +11,24 @@ import {
     UserProfileOrAuthButton,
 } from '@/widgets/user/button/UserProfileOrAuthButton/ui/UserProfileOrAuthButton.tsx';
 import { SiteLogoLinkTitle } from '@/entities/site/logo/SiteLogoLinkTitle.tsx';
+import {
+    DropdownModal,
+} from '@/shared/ui-kit/modal/DropdownModal/ui/DropdownModal.tsx';
+import { Button } from '@/shared/ui-kit/buttons/Button/ui/Button.tsx';
+import { IoNotifications } from 'react-icons/io5';
+import {
+    UserAuthFormWithUsernameByJsonServer,
+} from '@/widgets/user/form/UserAuthFormWithUsernameByJsonServer/ui/UserAuthFormWithUsernameByJsonServer.tsx';
+import {
+    useModalController,
+} from '@/shared/ui-kit/modal/Modal/hooks/useModalController.ts';
 
 
 export type HeaderNavBarProps = {};
 
 export const HeaderNavBar: FC<HeaderNavBarProps> = memo(function HeaderNavBar (props) {
-    const {} = props;
+    const {}              = props;
+    const modalController = useModalController();
 
     return (
         <div className={ css.container }>
@@ -27,6 +39,16 @@ export const HeaderNavBar: FC<HeaderNavBarProps> = memo(function HeaderNavBar (p
                 </li>
                 <li>
                     <ToggleThemeButton/>
+                </li>
+                <li>
+                    <DropdownModal
+                        controller={ modalController }
+                        dropdown={ <UserAuthFormWithUsernameByJsonServer/> }
+                    >
+                        <Button
+                            quad
+                            onClick={ () => modalController.setOpened((prev) => !prev) }><IoNotifications/></Button>
+                    </DropdownModal>
                 </li>
                 <li>
                     <UserProfileOrAuthButton/>
