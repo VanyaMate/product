7ea56225e898e5c1229ef2f520d10856111289b8
VanyaMate <buhu653@mail.ru>
2024-08-30 09:05:42 +0300

    change ru to en from translations paths

--- src/features/i18n/config/translations.ts
+++ src/features/i18n/config/translations.ts
@@ -1,37 +1,37 @@
 import { TranslationLanguage } from '@/features/i18n/types/language.ts';
 import { Translation } from '@/features/i18n/types/translations.ts';
 import ru_app from '@/app/i18n/translations/ru/site-app.json';
-import en_app from '@/app/i18n/translations/ru/site-app.json';
+import en_app from '@/app/i18n/translations/en/site-app.json';
 import ru_buttons from '@/app/i18n/translations/ru/buttons.json';
-import en_buttons from '@/app/i18n/translations/ru/buttons.json';
+import en_buttons from '@/app/i18n/translations/en/buttons.json';
 import ru_validation from '@/app/i18n/translations/ru/validation-messages.json';
-import en_validation from '@/app/i18n/translations/ru/validation-messages.json';
+import en_validation from '@/app/i18n/translations/en/validation-messages.json';
 import ru_contacts from '@/app/i18n/translations/ru/contacts.json';
-import en_contacts from '@/app/i18n/translations/ru/contacts.json';
+import en_contacts from '@/app/i18n/translations/en/contacts.json';
 import ru_files from '@/app/i18n/translations/ru/files-page.json';
-import en_files from '@/app/i18n/translations/ru/files-page.json';
+import en_files from '@/app/i18n/translations/en/files-page.json';
 import ru_dialogues from '@/app/i18n/translations/ru/dialogue.json';
-import en_dialogues from '@/app/i18n/translations/ru/dialogue.json';
+import en_dialogues from '@/app/i18n/translations/en/dialogue.json';
 import ru_posts from '@/app/i18n/translations/ru/posts.json';
-import en_posts from '@/app/i18n/translations/ru/posts.json';
+import en_posts from '@/app/i18n/translations/en/posts.json';
 import ru_friends from '@/app/i18n/translations/ru/friends-page.json';
-import en_friends from '@/app/i18n/translations/ru/friends-page.json';
+import en_friends from '@/app/i18n/translations/en/friends-page.json';
 import ru_languages from '@/app/i18n/translations/ru/languages.json';
-import en_languages from '@/app/i18n/translations/ru/languages.json';
+import en_languages from '@/app/i18n/translations/en/languages.json';
 import ru_userSettings from '@/app/i18n/translations/ru/user-settings.json';
-import en_userSettings from '@/app/i18n/translations/ru/user-settings.json';
+import en_userSettings from '@/app/i18n/translations/en/user-settings.json';
 import ru_notification_title
     from '@/app/i18n/translations/ru/notification-messages.json';
 import en_notification_title
-    from '@/app/i18n/translations/ru/notification-messages.json';
+    from '@/app/i18n/translations/en/notification-messages.json';
 import ru_notification_message
     from '@/app/i18n/translations/ru/notification-links.json';
 import en_notification_message
-    from '@/app/i18n/translations/ru/notification-links.json';
+    from '@/app/i18n/translations/en/notification-links.json';
 import ru_notification_helpers
     from '@/app/i18n/translations/ru/notifications.json';
 import en_notification_helpers
-    from '@/app/i18n/translations/ru/notifications.json';
+    from '@/app/i18n/translations/en/notifications.json';
 
 
 export const translations: Record<TranslationLanguage, Translation> = {
