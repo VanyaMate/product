b2612d21e8a66f5153fb590b081f8354d2985f2f
VanyaMate <buhu653@mail.ru>
2025-03-25 16:45:16 +0300

    🔥 New feature : App - Добавил commiter c настройками %build%tg_notify%unit%

---
+++ commiter/index.ts
@@ -0,0 +1,7 @@
+import { Commiter } from '@vanyamate/commiter';
+import options from './options';
+
+
+console.log('Index.tsx');
+
+new Commiter(options).create();
---
+++ commiter/options.ts
@@ -0,0 +1,43 @@
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import type { CommiterOptions } from '@vanyamate/commiter';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = resolve(__filename, '..');
+const gitFolder  = resolve(__dirname, '..');
+
+
+export default {
+    types                  : [ '❤️ Update', '🙏 Fix', '🔥 New feature' ],
+    entities               : [
+        'App',
+        'UI Kit',
+        'Authorization',
+        'Call',
+        'Comment',
+        'Connection',
+        'Files',
+        'Friends',
+        'Language',
+        'Message',
+        'Notification',
+        'Post',
+        'Dialogues',
+        'User',
+    ],
+    postfixes              : {
+        'Unit tests'       : 'unit',
+        'Build test'       : 'build',
+        'Playwright test'  : 'pw_test',
+        'Playwright update': 'pw_update',
+        'Cypress test'     : 'cy_test',
+        'Cypress update'   : 'cy_update',
+        'TG notify'        : 'tg_notify',
+    },
+    postfixesSeparator     : '%',
+    pattern                : `{{type}} : {{entities}} - {{message}} {{%postfixes%}}`,
+    gitFolder              : gitFolder,
+    gitRemoteRepositoryName: 'origin',
+    gitPushDefault         : true,
+} as CommiterOptions;
