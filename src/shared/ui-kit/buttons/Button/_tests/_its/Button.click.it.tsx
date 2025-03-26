d93396c625b4473170e55f3339a2f2725b577002
VanyaMate <buhu653@mail.ru>
2025-03-26 19:33:45 +0300

    ❤️ Update : Utils - Обновил тест для Button - Button.click

--- src/shared/ui-kit/buttons/Button/_tests/_its/Button.click.it.tsx
+++ src/shared/ui-kit/buttons/Button/_tests/_its/Button.click.it.tsx
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { Button } from '../../ui/Button';
 
 
 export default () => {
-    render(<Button>Simple</Button>);
-    expect(screen.getByText('Simple')).toBeInTheDocument();
+    const clickHandler  = jest.fn();
+    const { getByRole } = render(
+        <Button onClick={ clickHandler }>Simple</Button>,
+    );
+    const button        = getByRole('button');
+
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+    expect(clickHandler).toHaveBeenCalledTimes(1);
+    fireEvent.click(button);
+    expect(clickHandler).toHaveBeenCalledTimes(2);
 }
